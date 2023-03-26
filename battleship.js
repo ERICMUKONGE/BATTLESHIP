452a6d9267a3689ef46bb86741e495a08a7843be
MUKONGE ERIC <mukonge.eric@gmail.com>
2023-03-26 10:25:43 +0300

    Update

--- battleship.js
+++ battleship.js
@@ -14,5 +14,8 @@ while (isSunk == false){
         alert("Please enter a valid cell number!");
     } else {
         guesses = guesses + 1;
+        if (guess == location1 || guess == location2 || guess == location3){
+            hits = hits + 1;
+        }
     }
 }
