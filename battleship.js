48e9ebdf12a2e0120ebb5ce5671481cc268eb1d8
MUKONGE ERIC <mukonge.eric@gmail.com>
2023-03-26 11:51:25 +0300

    Update

--- battleship.js
+++ battleship.js
@@ -16,6 +16,10 @@ while (isSunk == false){
         guesses = guesses + 1;
         if (guess == location1 || guess == location2 || guess == location3){
             hits = hits + 1;
+            if (hits == 3){
+                isSunk = true;
+                alert('You sank my battleship!');
+            }
         }
     }
 }
