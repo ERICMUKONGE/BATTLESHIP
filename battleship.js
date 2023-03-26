386607347397c308ce5bf944881dea60acd72328
MUKONGE ERIC <mukonge.eric@gmail.com>
2023-03-26 12:00:16 +0300

    Update

--- battleship.js
+++ battleship.js
@@ -23,3 +23,6 @@ while (isSunk == false){
         }
     }
 }
+var stats = "You took" + guesses + "guesses to sink the battleship," +
+                "which means your shooting accuracy was " + (3/guesses);
+alert(stats);                
