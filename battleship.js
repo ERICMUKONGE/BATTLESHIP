e2e0e796e83ad77c5b9b0dfa9b33bff1d6732a7c
MUKONGE ERIC <mukonge.eric@gmail.com>
2023-03-26 12:08:40 +0300

    Update

--- battleship.js
+++ battleship.js
@@ -1,25 +1,27 @@
 var location1 = 3;
 var location2 = 4;
 var location3 = 5;
-
 var guess;
 var hits = 0;
 var guesses =0;
 var isSunk = false;
 
 while (isSunk == false){
-
     guess = prompt("Ready, aim, fire! (enter a number 0-6):");
     if  (guess < 0 || guess > 6){
         alert("Please enter a valid cell number!");
     } else {
         guesses = guesses + 1;
+
         if (guess == location1 || guess == location2 || guess == location3){
+            alert("HIT!");
             hits = hits + 1;
             if (hits == 3){
                 isSunk = true;
                 alert('You sank my battleship!');
             }
+        } else{
+            alert("MISS");
         }
     }
 }
